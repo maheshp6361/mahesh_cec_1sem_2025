ca784e5c54d2b2bbb9219b1525599a8be7656a07
maheshp6361 <maheshpatel85359@gmail.com>
2025-11-20 15:16:38 +0530

    day4 code pushed

---
+++ DAY4/2_variable.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int age = 30;
+    printf("Your age is : %d\n", age);
+    float marks = 345.34;
+    printf("Your marks are : %f\n", marks);
+    char ch = 'a';
+    printf("charachter = %c\n", ch);
+    double v = 123.345;
+    printf("double =%lf\n", v);
+    char name[50] = "xyz";
+    printf("name is :%s\n", name);
+    bool iswaliking = true;
+    printf("\n is waliking: %d", iswaliking);
+    return 0;
+}
